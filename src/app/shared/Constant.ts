2392655acfab9517c500634a23a0b35abb80f1c6
Jai Trinity <jai.prakash@trinityapplab.co.in>
2023-09-04 12:48:00 +0530

    Add new branch

--- src/app/shared/Constant.ts
+++ src/app/shared/Constant.ts
@@ -2,4 +2,5 @@ export class Constant{
     public static APP_URL = "http://www.trinityapplab.co.in/UniversalApp/";
     public static SUCCESS_CODE   = "100000";
     public static TRINITY_PRIVATE_KEY = "TRINITYPRIVATEKEY";
+    public static COMPANY = "Universal App"
 }
