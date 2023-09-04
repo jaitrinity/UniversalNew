b374a5fced1ed008da2715d6e0df473020d04489
Jai Trinity <jai.prakash@trinityapplab.co.in>
2023-09-04 13:01:31 +0530

    remove comment

--- src/app/shared/Constant.ts
+++ src/app/shared/Constant.ts
@@ -3,4 +3,8 @@ export class Constant{
     public static SUCCESS_CODE   = "100000";
     public static TRINITY_PRIVATE_KEY = "TRINITYPRIVATEKEY";
     public static COMPANY = "Universal App"
+
+    public static returnServerErrorMessage(serviceName:string):string{
+        return "Server error while invoking "+serviceName+ " service";
+    }
 }
